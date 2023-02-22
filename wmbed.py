f16931718477eee047478d8f7a259a2edb9609d1
genericv <viktor.bordyuzha@gmail.com>
2023-02-22 20:30:48 +0200

    Fix: make padding relative

--- wmbed.py
+++ wmbed.py
@@ -12,46 +12,24 @@ def scale_watermark(watermark, image_size, relative_scale):
     return resized
 
 
-def apply_threshold(value, threshold):
-    if value > threshold:
-        return threshold
-    else:
-        return value
-
-
-def embed_positional_watermark(image, watermark, position, scale, opacity, padding):
+def embed_positional_watermark(image, watermark, position, scale, opacity, relative_padding):
     image_height, image_width = image.shape[:2]
     watermark = scale_watermark(watermark, (image_width, image_height), scale)
     watermark_height, watermark_width = watermark.shape[:2]
-    padding = apply_threshold(padding, min(image_width - watermark_width, image_height - watermark_height))
+    padding_limit = min(image_width - watermark_width, image_height - watermark_height)
+    padding = int(padding_limit * relative_padding)
     horizontal_bounds = ()
     vertical_bounds = ()
     vertical_position = position[0]
     horizontal_position = position[1]
     if vertical_position == "T":
-        if watermark_height + padding <= image_height:
-            vertical_bounds = (padding, watermark_height + padding)
-        else:
-            vertical_bounds = (padding, image_height)
-            watermark = watermark[:image_height - padding, ...]
+        vertical_bounds = (padding, watermark_height + padding)
     elif vertical_position == "B":
-        if watermark_height + padding <= image_height:
-            vertical_bounds = (image_height - watermark_height - padding, image_height - padding)
-        else:
-            vertical_bounds = (0, image_height - padding)
-            watermark = watermark[watermark_height + padding - image_height:, ...]
+        vertical_bounds = (image_height - watermark_height - padding, image_height - padding)
     if horizontal_position == "L":
-        if watermark_width + padding <= image_width:
-            horizontal_bounds = (padding, watermark_width + padding)
-        else:
-            horizontal_bounds = (padding, image_width)
-            watermark = watermark[..., image_width - padding]
+        horizontal_bounds = (padding, watermark_width + padding)
     elif horizontal_position == "R":
-        if watermark_width + padding <= image_width:
-            horizontal_bounds = (image_width - watermark_width - padding, image_width - padding)
-        else:
-            horizontal_bounds = (0, image_width - padding)
-            watermark = watermark[..., watermark_width + padding - image_width]
+        horizontal_bounds = (image_width - watermark_width - padding, image_width - padding)
     marked_image = embed(image, watermark, horizontal_bounds, vertical_bounds, opacity)
     return marked_image
 
@@ -72,8 +50,8 @@ def create_image_with_positional_watermark(
         position="BR",
         scale=1.0,
         opacity=0.4,
-        padding=0):
+        relative_padding=0):
     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
     watermark = cv2.imread(watermark_path, cv2.IMREAD_COLOR)
-    marked_image = embed_positional_watermark(image, watermark, position, scale, opacity, padding)
+    marked_image = embed_positional_watermark(image, watermark, position, scale, opacity, relative_padding)
     cv2.imwrite(save_path, marked_image)

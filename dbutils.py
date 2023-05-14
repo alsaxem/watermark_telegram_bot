e7a924d51dc4e047415c2352206ac74d98656871
alsaxem <alsaxem2015@gmail.com>
2023-05-14 04:38:37 +0300

    Feature: add an angle field in the add_user function

--- dbutils.py
+++ dbutils.py
@@ -38,8 +38,9 @@ def get_all_info(user_id):
 
 
 def add_user(*args):
-    c.execute("INSERT OR REPLACE INTO users (user_id, name, watermark_id, position, scale, opacity, padding, language)"
-              "VALUES (?, ?, NULL, NULL, NULL, NULL, NULL, NULL)", args)
+    c.execute("INSERT OR REPLACE INTO users (user_id, name, watermark_id, position, scale, opacity, "
+              "padding, angle ,language)"
+              "VALUES (?, ?, NULL, NULL, NULL, NULL, NULL, NULL, NULL)", args)
     conn.commit()
 
 

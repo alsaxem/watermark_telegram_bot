ea38891bbbd6ee129dd1f2c97654dc66aa51dd45
alsaxem <alsaxem2015@gmail.com>
2023-05-15 14:57:13 +0300

    Feature: use threading lock to lock the sqlite cursor object

--- dbutils.py
+++ dbutils.py
@@ -1,5 +1,7 @@
 import sqlite3
+import threading
 
+lock = threading.Lock()
 db_name = "bot.db"
 
 conn = sqlite3.connect(database=db_name, check_same_thread=False)
@@ -7,45 +9,51 @@ c = conn.cursor()
 
 
 def create_users_table():
-    c.execute("CREATE TABLE IF NOT EXISTS users ("
-              "user_id INTEGER PRIMARY KEY NOT NULL,"
-              "name TEXT NOT NULL,"
-              "watermark_id TEXT,"
-              "position TEXT,"
-              "scale FLOAT,"
-              "opacity FLOAT,"
-              "angle INTEGER, "
-              "padding FLOAT,"
-              "language TEXT)")
+    with lock:
+        c.execute("CREATE TABLE IF NOT EXISTS users ("
+                  "user_id INTEGER PRIMARY KEY NOT NULL,"
+                  "name TEXT NOT NULL,"
+                  "watermark_id TEXT,"
+                  "position TEXT,"
+                  "scale FLOAT,"
+                  "opacity FLOAT,"
+                  "angle INTEGER, "
+                  "padding FLOAT,"
+                  "language TEXT)")
 
 
 def drop_table(table_name="users"):
-    c.execute("DROP TABLE " + table_name)
+    with lock:
+        c.execute("DROP TABLE " + table_name)
 
 
 def get_fields_info(user_id, fields):
-    c.execute("SELECT " + fields + " FROM users WHERE user_id=?", (user_id,))
-    data = c.fetchone()
+    with lock:
+        c.execute("SELECT " + fields + " FROM users WHERE user_id=?", (user_id,))
+        data = c.fetchone()
     if data:
         data = list(data)
     return data
 
 
 def get_all_info(user_id):
-    c.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
-    return c.fetchone()
+    with lock:
+        c.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
+        return c.fetchone()
 
 
 def get_all_users():
-    c.execute("SELECT user_id FROM users")
-    return [item[0] for item in c.fetchall()]
+    with lock:
+        c.execute("SELECT user_id FROM users")
+        return [item[0] for item in c.fetchall()]
 
 
 def add_user(*args):
-    c.execute("INSERT OR REPLACE INTO users (user_id, name, watermark_id, position, scale, opacity, "
-              "padding, angle, language)"
-              "VALUES (?, ?, NULL, NULL, NULL, NULL, NULL, NULL, NULL)", args)
-    conn.commit()
+    with lock:
+        c.execute("INSERT OR REPLACE INTO users (user_id, name, watermark_id, position, scale, opacity, "
+                  "padding, angle, language)"
+                  "VALUES (?, ?, NULL, NULL, NULL, NULL, NULL, NULL, NULL)", args)
+        conn.commit()
 
 
 def update_info(user_id, column, value):
@@ -53,8 +61,9 @@ def update_info(user_id, column, value):
         value = "\"" + value + "\""
     if value is None:
         value = "NULL"
-    c.execute("UPDATE users SET " + column + "=" + str(value) + " WHERE user_id = " + str(user_id))
-    conn.commit()
+    with lock:
+        c.execute("UPDATE users SET " + column + "=" + str(value) + " WHERE user_id = " + str(user_id))
+        conn.commit()
 
 
 def is_user_exist(user_id):

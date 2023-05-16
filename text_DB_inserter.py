14d182ce15f0624539a67f3f3dff316fddef21fe
alsaxem <alsaxem2015@gmail.com>
2023-05-16 21:54:22 +0300

    Feature: splitting the code into functions and creating the control function start

--- text_DB_inserter.py
+++ text_DB_inserter.py
@@ -1,24 +1,23 @@
 import sqlite3
 from config import db_name, dictionary_name, languages
 
-conn = sqlite3.connect(db_name)
-cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS texts
-                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   title TEXT UNIQUE)''')
+def create_tables(cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS texts
+                      (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       title TEXT UNIQUE)''')
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS languages
-                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   name TEXT UNIQUE)''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS languages
+                      (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       name TEXT UNIQUE)''')
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS text_languages
-                  (text_id INTEGER,
-                   lang_id INTEGER,
-                   content TEXT,
-                   FOREIGN KEY (text_id) REFERENCES texts (id),
-                   FOREIGN KEY (lang_id) REFERENCES languages (id),
-                   PRIMARY KEY (text_id, lang_id))''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS text_languages
+                      (text_id INTEGER,
+                       lang_id INTEGER,
+                       content TEXT,
+                       FOREIGN KEY (text_id) REFERENCES texts (id),
+                       FOREIGN KEY (lang_id) REFERENCES languages (id),
+                       PRIMARY KEY (text_id, lang_id))''')
 
 
 def parse_data(file_name):
@@ -45,31 +44,41 @@ def parse_data(file_name):
     return data
 
 
-texts = parse_data(dictionary_name)
-
-for text in texts:
-    cursor.execute('SELECT id FROM texts WHERE title = ?', (text['title'],))
-    result = cursor.fetchone()
-
-    if result:
-        text_id = result[0]
-    else:
-        cursor.execute('INSERT OR REPLACE INTO texts (title) VALUES (?)', (text['title'],))
-        text_id = cursor.lastrowid
-
-    for lang in languages:
-        cursor.execute('SELECT id FROM languages WHERE name = ?', (lang,))
+def insert_text(cursor, texts):
+    for text in texts:
+        cursor.execute('SELECT id FROM texts WHERE title = ?', (text['title'],))
         result = cursor.fetchone()
 
         if result:
-            lang_id = result[0]
+            text_id = result[0]
         else:
-            cursor.execute('INSERT OR REPLACE INTO languages (name) VALUES (?)', (lang,))
-            lang_id = cursor.lastrowid
+            cursor.execute('INSERT OR REPLACE INTO texts (title) VALUES (?)', (text['title'],))
+            text_id = cursor.lastrowid
 
-        content = text['content'][lang]
-        cursor.execute('INSERT OR REPLACE INTO text_languages (text_id, lang_id, content) VALUES (?, ?, ?)', (text_id, lang_id, content))
+        for lang in languages:
+            cursor.execute('SELECT id FROM languages WHERE name = ?', (lang,))
+            result = cursor.fetchone()
+
+            if result:
+                lang_id = result[0]
+            else:
+                cursor.execute('INSERT OR REPLACE INTO languages (name) VALUES (?)', (lang,))
+                lang_id = cursor.lastrowid
+
+            content = text['content'][lang]
+            cursor.execute('INSERT OR REPLACE INTO text_languages (text_id, lang_id, content) VALUES (?, ?, ?)',
+                           (text_id, lang_id, content))
 
 
-conn.commit()
-conn.close()
+def start():
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    create_tables(cursor)
+    texts = parse_data(dictionary_name)
+    insert_text(cursor, texts)
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    start()

618efef958d029ed786677b02094fbba077a692c
alsaxem <alsaxem2015@gmail.com>
2023-05-19 18:39:09 +0300

    Feature: add the position_values to the config file

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ empty_value = None
 languages = ['en', 'ua', 'ru']
 settings = ["watermark_id", "position", "scale", "opacity", "padding", "angle", "language"]
 position_values = ["TL", "TC", "TR", "CL", "CC", "CR", "BL", "BC", "BR", "FILLING"]
+position_prints = ["↖", "⬆", "↗", "⬅", "⏺", "➡", "↙", "⬇", "↘", "position_FILLING"]
 scale_values = ["x0.1", "x0.2", "x0.3", "x0.4", "x0.5", "x0.6", "x0.7", "x0.8", "x0.9", "x1.0"]
 opacity_values = ["0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1.0"]
 padding_values = ["0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"]
